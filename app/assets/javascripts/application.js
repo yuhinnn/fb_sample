75de74ca8786f10ec0ee5e5e43943b4fa08421d6
yuhi_deve <yuhinnn@gmail.com>
2016-05-01 02:01:35 +0900

    ヒロと統合（hover解決せず）

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,22 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '508134929381636',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+
+    // ADD ADDITIONAL FACEBOOK CODE HERE
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
