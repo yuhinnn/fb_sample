5ebcbb577380742af73129370e2eea984e81bf9b
yuhi_deve <yuhinnn@gmail.com>
2016-05-05 18:47:53 +0900

    rm -rf public/assets & rm end*2 from config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
 
   end
 
-end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -61,4 +60,3 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
