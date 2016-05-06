6983aa7e3ce2478228b492f812d16ef60bc6a025
yuhi_deve <yuhinnn@gmail.com>
2016-05-06 14:14:14 +0900

    add config.action dispatch ~ :25 in locales application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module FbSample
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_dispatch.default_headers['X-Frame-Options'] = "Allow-From https://apps.facebook.com"
   end
 end
